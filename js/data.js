dccc7cde439c3a77d891365ef12373fe5dd763b3
Korneliu <olbiton@gmail.com>
2021-10-26 14:54:16 -0700

    completed issue-1-create-an-entry

--- js/data.js
+++ js/data.js
@@ -4,5 +4,12 @@ var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
-  nextEntryId: 1
+  nextEntryId: 0
 };
+
+function handleBeforUnload() {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', handleBeforUnload);
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
 var $photoUrl = document.querySelector('#photo-url');
 var $image = document.querySelector('.image');
+var $form = document.querySelector('#form');
 
 function handlePhotoUrl(event) {
   var imageUrl = event.target.value;
@@ -10,3 +11,23 @@ function handlePhotoUrl(event) {
 }
 
 $photoUrl.addEventListener('input', handlePhotoUrl);
+
+function handleForm(event) {
+  event.preventDefault();
+  var inputObject = {
+    title: '',
+    photo: '',
+    notes: '',
+    entryId: 0
+  };
+  inputObject.title = event.target.title.value;
+  inputObject.photo = event.target.photo.value;
+  inputObject.notes = event.target.notes.value;
+  data.nextEntryId++;
+  inputObject.entryId = data.nextEntryId;
+  data.entries.unshift(inputObject);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$form.addEventListener('submit', handleForm);
