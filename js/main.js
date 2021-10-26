d495bdb13e258c02919b06647054b982e16d7996
Korneliu <olbiton@gmail.com>
2021-10-26 11:27:05 -0700

    added addEventListener for photoUrl

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photo-url');
+var $image = document.querySelector('.image');
+
+function handlePhotoUrl(event) {
+  var imageUrl = event.target.value;
+  $image.setAttribute('src', imageUrl);
+}
+
+$photoUrl.addEventListener('input', handlePhotoUrl);
